ceac503c9c9bbd9a248065689b8617903b7af958
paradoxfly <timothyogwulumba@gmail.com>
2022-01-30 14:56:23 +0100

    Adjusted folder structure
    Adjusted folder structure of the program.
    Moved classes and functions to their own files.
    Deleted redundancies

---
+++ javascript/main.js
@@ -0,0 +1,68 @@
+//Next button
+const nextBtn = document.getElementById("next");
+nextBtn.addEventListener("click", (event) => {
+    quiz.questionIndex++;
+    if(quiz.questionIndex == questions.length - 1){
+        nextBtn.classList.add("hide");
+    }
+    displayQuestion();
+});
+
+//Previous button
+const prevBtn = document.getElementById("prev");
+prevBtn.addEventListener("click", (event)=>{
+    quiz.questionIndex--;
+    displayQuestion();
+});
+
+//Submit button
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", (event) => {
+    showScores();
+});
+
+//Guess fxn
+function guess(id,guess){
+    let button = document.getElementById(id);
+    button.onclick = function(){
+       quiz.guess(guess);
+       displayQuestion(); 
+    }
+}
+
+
+//an array of questions from a Database
+const data = [
+    {
+        que: "Which is the backend of Python?",
+        arr: ["Nodejs","Django","Ruby","Asp.Net"],
+        ans:"Ruby"
+    },
+    {
+        que: "What is the full Meaning of PHP?",
+        arr: ["javascript","Cascade style","Ruby","Python"],
+        ans:"javascript"
+    },
+    {
+        que: "Which language is built on React?",
+        arr: ["javascript","Cascade style","Ruby","Python"],
+        ans:"javascript"
+    },
+    {
+        que: "What is the full Meaning of PHP?",
+        arr: ["javascript","Cascade style","Ruby","Python"],
+        ans:"javascript"
+    },
+]
+
+//Create quiz questions
+let questions = data.map(item => new Question(item.que,item.arr,item.ans));
+
+//Create new quiz class instance
+let quiz = new Quiz(questions);
+
+//display questions
+displayQuestion();
+
+//Start countdown
+startCountdown();
---
+++ javascript/utils/display.js
@@ -0,0 +1,54 @@
+//Display question
+function displayQuestion(){
+  if(quiz.questionIndex < questions.length){
+      submitBtn.classList.add("hide");
+  }
+  if(quiz.questionIndex == 0) {
+      prevBtn.classList.add("hide");
+  }
+  if(quiz.questionIndex >= 1){
+      prevBtn.classList.remove("hide");
+  }
+  if(quiz.questionIndex == questions.length - 1){
+      submitBtn.classList.remove("hide");
+  }
+  if(quiz.isEnded()){
+      showScores();
+  }else{
+      let questionElement = document.getElementById("question_text");
+      questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+      //show options
+      let choices = quiz.getQuestionIndex().choices;
+      for(let i = 0; i < choices.length; i++){
+          let choiceElement = document.getElementById("choice" + i);
+          choiceElement.innerHTML = choices[i];
+          guess("btn" + i, choices[i]);
+      }
+
+      showProgress();
+  }
+}
+
+//show progress
+function showProgress(){
+  let currentQuestionNumber = quiz.questionIndex + 1;
+  let progressElement = document.getElementById("question_status");
+  progressElement.innerHTML = `
+      Question ${currentQuestionNumber} of ${quiz.questions.length}
+  `;
+}
+
+//show score
+function showScores(){
+  alert.innerText = "";
+  let quizEndHTML = `
+      <h1>Quiz Completed</h1>
+      <h2 id="score">You Scored: ${quiz.score} of ${quiz.questions.length} </h2>
+      <div class="quiz-repeat">
+          <a href="index.html">Take Quiz Again</a>
+      </div>
+      `;
+      let quizElement = document.getElementById("contentt");
+      quizElement.innerHTML = quizEndHTML;
+}
